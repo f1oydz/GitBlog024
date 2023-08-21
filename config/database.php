b372d6772631b33c1accc5541504b3e6d76c3478
f1oydz <6514120014@mut.ac.th>
2023-08-21 23:03:05 +0700

    lab8_014

--- config/database.php
+++ config/database.php
@@ -44,13 +44,13 @@ return [
         ],
 
 
-        
+
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'blog024'),
+            'database' => env('DB_DATABASE', 'blog014'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
